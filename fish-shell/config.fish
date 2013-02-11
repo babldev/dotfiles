46977eb74c41424b54cd615f7ec2110781aa1044
Brady Law <bradyl16@gmail.com>
2013-02-11 14:06:18 -0800

    Link mongodb bin path

--- fish-shell/config.fish
+++ fish-shell/config.fish
@@ -4,3 +4,5 @@ set -x PATH "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin" $PATH
 # Link the git bin.
 set -x PATH "/usr/local/git/bin" $PATH
 
+# Link the mongodb bin
+set -x PATH "/usr/local/mongodb/bin" $PATH
